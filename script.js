9427fb35258e89db6a11f911ae678f01272cee35
Jan Sklepek <16152498+hsklpk@users.noreply.github.com>
2024-03-04 10:56:42 +0100

    2. verze projektu

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
 const menuIcon = document.querySelector(".menu-icon");
-// console.log(menuIcon);
 const menuList = document.querySelector("nav");
-// console.log(menuList);
+const main = document.querySelector("welcome");
 const hamburgerIcon = document.querySelector(".fa-solid");
-// console.log(hamburgerIcon.classList);
+const hamburgerIconClasses = hamburgerIcon.classList;
 
 menuIcon.addEventListener("click", () => {
-  if (hamburgerIcon.classList[1] === "fa-bars") {
-    hamburgerIcon.classList.remove("fa-bars");
-    hamburgerIcon.classList.add("fa-xmark");
-    menuList.style.display = "block";
+  if (hamburgerIconClasses.contains("fa-bars")) {
+    hamburgerIconClasses.replace("fa-bars", "fa-xmark");
+    menuList.style.width = "250px";
   } else {
-    hamburgerIcon.classList.remove("fa-xmark");
-    hamburgerIcon.classList.add("fa-bars");
-    menuList.style.display = "none";
+    hamburgerIconClasses.replace("fa-xmark", "fa-bars");
+    menuList.style.width = "0";
   }
 });
