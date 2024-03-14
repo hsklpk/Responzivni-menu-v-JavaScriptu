514a36a7b1d4c7695c5f187416cb00019fbfa47f
Jan Sklepek <16152498+hsklpk@users.noreply.github.com>
2024-03-14 16:56:19 +0100

    Update script.js
    Verze 2 - kratší kód JS

--- script.js
+++ script.js
@@ -1,15 +1,9 @@
 const menuIcon = document.querySelector(".menu-icon");
-const menuList = document.querySelector("nav");
-const main = document.querySelector("welcome");
 const hamburgerIcon = document.querySelector(".fa-solid");
-const hamburgerIconClasses = hamburgerIcon.classList;
+const menuList = document.querySelector("nav");
 
 menuIcon.addEventListener("click", () => {
-  if (hamburgerIconClasses.contains("fa-bars")) {
-    hamburgerIconClasses.replace("fa-bars", "fa-xmark");
-    menuList.style.width = "250px";
-  } else {
-    hamburgerIconClasses.replace("fa-xmark", "fa-bars");
-    menuList.style.width = "0";
-  }
+    hamburgerIcon.classList.toggle("fa-bars");
+    hamburgerIcon.classList.toggle("fa-xmark");
+    menuList.classList.toggle("active");
 });
